cf0e83d378b90f1150b82ff1a160cc7145fca16f
Rubens Lima <rubens.aurenio@gmail.com>
2023-09-21 19:29:40 -0300

    Segundo Commit do Intensivão de JavaScript após NPM RUN Build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/magazineIntensivao/",
+  base: "/Magazine_Intensivao_JavaScript/",
   plugins: [],
   resolve: {
     /*something*/
